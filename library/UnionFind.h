0d8891bfed3b89d10a19c904908115a67d02b002
Pedro2um1dois <pedroigorgm@gmail.com>
2022-10-16 18:46:04 -0300

    Documentado

--- library/UnionFind.h
+++ library/UnionFind.h
@@ -3,19 +3,43 @@
 
 typedef struct uf UF;
 
-// incia o conjunto/árvore/grafo que utiliza do algoritimo de Union-Find, os elementos são representados por numeros que começam do 1 e vão até N (inclusivo)
+/**
+ * @brief Incia o conjunto
+ * @param O numero de elementos do conjunto
+ * @return Um ponteiro para um conjunto
+ */
+//os elementos são representados por numeros que começam do 1 e vão até N (inclusivo)
 UF* initUnionFind(unsigned short n);
 
-//une dois elementos que são representados por inteiros na estrutura
+/**
+ * @brief Une dois elementos que são representados por inteiros na estrutura
+ * @param Dois inteiros
+ * @return Nenhum
+ */
 void Union(UF* uf,  unsigned short a, unsigned short b);
 
-//verifica se dois elementos estao conectados)
+/**
+ * @brief   Verifica se dois elementos estao conectados
+ * @param   Dois inteiros
+ * @return  Nenhum
+ */
+//
 unsigned short Conected(UF* uf, unsigned short a, unsigned short b);
 
-//acha o pai do elemento especificado
+/**
+ * @brief   Acha o pai do elemento especificado
+ * @param   Um ponteiro para um conjunto alocado
+ * @param   Um inteiro
+ * @return  Nenhum
+ */
 unsigned short Find(UF* uf, unsigned short a);
 
-//libera toda a estrutura do union find
+/**
+ * @brief   Libera toda a estrutura do conjunto
+ * @param   Um ponteiro para um conjunto alocado
+ * @return  Nenhum
+ */
+//
 void freeUnionFind(UF* uf);
 
 #endif 
